e5f6ee5d6ed16237c5ecb7dec9f382e6a80f3831
Julian Jarosz <01197479@pw.edu.pl>
2025-11-07 16:36:29 +0100

    Update file test_getprocnr.c

--- ZADANIE 1/test_getprocnr.c
+++ ZADANIE 1/test_getprocnr.c
@@ -8,28 +8,22 @@ int getprocnr(int pid)
 {
     message m;
     m.m1_i1 = pid;
-    
     return _syscall(MM, GETPROCNR, &m);
 }
 
 int main(int argc, char *argv[])
 {
-
-
     int pidstart = atoi(argv[1]);
     int pid;
     int result;
-
-
-for(pid = pidstart; pid <= pidstart + 10; pid++){
+    for(pid = pidstart; pid <= pidstart + 10; pid++){
         errno = 0;
         result = getprocnr(pid);
-
         if (result < 0){
             printf("not found: %d , %d\n", pid, errno);
         } else {
             printf("found  %d , %d\n", pid, result);
         }
-        }
+    }
         return 0;
 }
